bd7f427580f095891bd9a8ae523657b0fdc52c64
rex.yeung <rex.yeung@gmail.com>
2019-12-18 15:18:01 +0800

    Add project files.

---
+++ VLCTest/VLCTest/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VLCTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+ 
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        void OnAppearing(object sender, System.EventArgs e)
+        {
+            base.OnAppearing();
+            ((MainViewModel)BindingContext).OnAppearing();
+        }
+
+
+    }
+}
